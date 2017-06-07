0fe354c2fc588850fb7adbff6d476347637781bd
ericmdai <emd88@cornell.edu>
2017-06-07 17:54:02 -0400

    Rearranging assets

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
 
-    game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.image('sky', 'assets/game/sky.png');
+    game.load.image('ground', 'assets/game/platform.png');
+    game.load.image('star', 'assets/game/star.png');
+    game.load.spritesheet('dude', 'assets/game/dude.png', 32, 48);
 
 }
 
